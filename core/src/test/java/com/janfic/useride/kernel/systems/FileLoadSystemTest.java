bcfeac3bcd97ea3ef3ab12840f3c92601de768d5
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-02-23 20:13:00 -0500

    fixed tests for FileLoadSystem - added backend libgdx dependency to useride:core Task #7 - Cannot test some components withut libGDX backend running

--- core/src/test/java/com/janfic/useride/kernel/systems/FileLoadSystemTest.java
+++ core/src/test/java/com/janfic/useride/kernel/systems/FileLoadSystemTest.java
@@ -2,7 +2,12 @@ package test.java.com.janfic.useride.kernel.systems;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.files.FileHandle;
+import com.janfic.useride.kernel.components.FileComponent;
 import com.janfic.useride.kernel.components.FileLoadRequestComponent;
+import com.janfic.useride.kernel.systems.FileLoadSystem;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,19 +17,28 @@ import org.junit.Test;
  * @author janfc
  */
 public class FileLoadSystemTest {
-    
+
+    static LwjglApplication app;
     static Engine engine;
     static Entity entity;
 
     @BeforeClass
     public static void setUpClass() {
 
+        app = new LwjglApplication(new Game() {
+            @Override
+            public void create() {
+
+            }
+        });
+
+        
         engine = new Engine();
         entity = new Entity();
 
         FileLoadRequestComponent loadRequest = new FileLoadRequestComponent();
         loadRequest.fileName = "tests/FileLoadSystemTest.txt";
-        
+
         entity.add(loadRequest);
         engine.addEntity(entity);
     }
@@ -38,9 +52,16 @@ public class FileLoadSystemTest {
      */
     @Test
     public void test() {
-        //FileLoadSystem instance = new FileLoadSystem();
-        //engine.addSystem(instance);
-        //engine.update(0);
+        FileLoadSystem instance = new FileLoadSystem();
+        engine.addSystem(instance);
+        engine.update(0);
+        
+        assert(entity.getComponent(FileComponent.class) != null);
+        assert(entity.getComponent(FileLoadRequestComponent.class) == null);
+        
+        FileHandle file = entity.getComponent(FileComponent.class).file;
+        
+        assert(file.name().equals("FileLoadSystemTest.txt"));
     }
 
 }
