71731a1777e90b99c37495ecc248eaff94266541
Jan Fic <Jan Fic@DESKTOP-UUHC5V6>
2020-08-31 13:07:52 -0400

    added basic files program structure added files program assets

---
+++ core/assets/files/systems/BootSystem.groovy
@@ -0,0 +1,22 @@
+package files.systems;
+
+import com.badlogic.ashley.core.*;
+
+public class BootSystem extends EntitySystem {
+    
+    public BootSystem() {
+        
+    }
+    
+    public void addedToEngine(Engine engine) {
+        
+    }
+    
+    public void update(float delta) {
+        
+    }
+    
+    public void removedFromEngine(Engine engine) {
+        
+    }
+}
