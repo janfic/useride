2ac28e1a4f348bdc91d1b6ef978421a66ef05fd9
janfic <jan.fic18@ncf.edu>
2020-09-30 20:11:58 -0400

    added splash screen

--- core/src/com/janfic/useride/USERIDEGame.java
+++ core/src/com/janfic/useride/USERIDEGame.java
@@ -5,16 +5,23 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.janfic.useride.kernel.systems.BootSystem;
 
 public class USERIDEGame extends Game {
 
     Engine engine;
-    
+    Texture splashTexture;
+    SpriteBatch batch;
+    float splashScreenWait = 3;
+
     @Override
     public void create() {
         engine = new Engine();
         Gdx.input.setInputProcessor(new InputMultiplexer());
+        splashTexture = new Texture("splash.png");
+        batch = new SpriteBatch();
         engine.addSystem(new BootSystem());
     }
 
@@ -22,7 +29,15 @@ public class USERIDEGame extends Game {
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        engine.update(Gdx.graphics.getDeltaTime());
+
+        if (splashScreenWait > 0) {
+            batch.begin();
+            batch.draw(splashTexture, 1980 / 2 - splashTexture.getWidth(), 1080 / 2 - splashTexture.getHeight(), splashTexture.getWidth() * 2, splashTexture.getHeight() * 2);
+            batch.end();
+            splashScreenWait -= Gdx.graphics.getDeltaTime();
+        } else {
+            engine.update(Gdx.graphics.getDeltaTime());
+        }
     }
 
     @Override
