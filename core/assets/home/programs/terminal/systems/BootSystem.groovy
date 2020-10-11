bf2184c9f5318273d7d9194f33ffb9f459599f50
janfic <jan.fic18@ncf.edu>
2020-10-11 01:20:07 -0400

    terminal program setup

---
+++ core/assets/home/programs/terminal/systems/BootSystem.groovy
@@ -0,0 +1,49 @@
+package terminal.systems;
+
+import com.janfic.useride.kernel.components.*;
+import com.janfic.useride.kernel.systems.*;
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.*;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.*;
+import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.assets.*;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.input.*;
+import com.badlogic.gdx.math.*;
+import os.components.*;
+import os.systems.*;
+
+
+public class BootSystem extends EntitySystem {	
+
+    public void addedToEngine(Engine engine) {
+        ImmutableArray<Entity> inject = engine.getEntitiesFor(Family.all(ProgramEntityInjectionComponent.class).get());
+        ProgramEntityInjectionComponent injection = inject.first().getComponent(ProgramEntityInjectionComponent.class);
+        
+        for(Entity e : injection.entities) {
+            engine.addEntity(e);
+        }
+               
+        Entity assetManagerEntity = new Entity();
+        AssetManagerComponent amComp = new AssetManagerComponent();
+        amComp.manager = new AssetManager();
+        assetManagerEntity.add(amComp);
+        
+        engine.removeSystem(this);
+    }
+    
+    public void update(float delta) {
+        
+    }
+    
+    public void removedFromEngine(Engine engine) {
+        
+    }
+    
+}
