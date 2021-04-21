a3b652759af4228927244bc3d80ecfb400d807e2
janfic <jan.fic18@ncf.edu>
2021-04-20 22:23:24 -0400

    added support for textarea tag in HTML/CSS

--- core/assets/home/programs/htmlTester/systems/BootSystem.groovy
+++ core/assets/home/programs/htmlTester/systems/BootSystem.groovy
@@ -63,6 +63,12 @@ public class BootSystem extends EntitySystem {
         engine.addSystem(new AssetRegisterSystem());
         engine.addSystem(new RelativePositionSystem());
         engine.addSystem(new ViewportSystem());
+        engine.addSystem(new KeyboardInputSystem())
+        engine.addSystem(new MouseClickSystem());
+        engine.addSystem(new MouseHoverSystem());
+        engine.addSystem(new TextInputSystem());
+        engine.addSystem(new FocusOnMouseClickSystem());
+        engine.addSystem(new ChangeBackgroundColorOnFocusedSystem());
 
         super.addedToEngine(engine);
         
